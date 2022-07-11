02f286960de41c1d23b4382ebc2f0685bff0335c
MarcCruzs <cramzurc1234@gmail.com>
2022-07-11 10:35:49 -0700

    Documenting
    Added some explanation to what this python code does.

--- code/ClassLabelFilter.py
+++ code/ClassLabelFilter.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import copy
 from csv import writer
-
+'''pd.read_csv is used to read a csv file that is supposed to have a collection of instances of differing labels/Targets
+that will be filtered through'''
 Main = pd.read_csv('MobilityScooterData.csv', names = ["Target", "X","Y","Z","GX","GY","GZ"])
 print(Main['Target'].value_counts())
 
@@ -18,6 +19,9 @@ SLT = copy.deepcopy(Main)
 SRT = copy.deepcopy(Main)
 SB = copy.deepcopy(Main)
 #Sudden Acceleration (1), Sudden Right Turn (2), Sudden Left Turn (3), Sudden Break (4), and Normal (5)
+'''How the filter function works is it starts with what you want to name the csv file. Then whichever corresponding
+array that is supposed to go with that csv file. Then value1-value4 are the values you want to get rid of. 
+NOTE: used pandas drop function to remove each row with a specific value for the column "Target."'''
 def Filter(name, lst, value1,value2,value3,value4):
     lst.drop(lst[lst['Target'] == value1].index, inplace = True)
     lst.drop(lst[lst['Target'] == value2].index, inplace = True)
